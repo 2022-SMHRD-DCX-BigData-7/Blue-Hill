72c144faa85745e338ab083d994ae061e2c424e3
smart <smart@smhrd>
2023-07-12 15:44:13 +0900

    수정 2차

--- gittest/src/main/java/gittest/test.java
+++ gittest/src/main/java/gittest/test.java
@@ -6,6 +6,11 @@ public class test {
 		System.out.println("팀장 : 이재승");
 
 		System.out.println("데이터모델링 : 황동휘");
+		System.out.println("백엔드 : 신민식");
+		System.out.println("백엔드 : 이창훈");
+		System.out.println("백엔드 : 우태현");
+		
+		
 	}
 
 }
